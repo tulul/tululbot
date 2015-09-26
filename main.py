3d6fb6ccb50fe38b3ef30c3182576449e343fa91
Muhammad Iqbal <muhammad.iqbal018@ui.ac.id>
2015-09-27 02:09:33 +0700

    initial commit bisa ngelely sama ngewiki

---
+++ main.py
@@ -0,0 +1,155 @@
+import StringIO
+import json
+import logging
+import random
+import urllib
+import urllib2
+
+# for sending images
+from PIL import Image
+import multipart
+from wiki import wiki
+
+# standard app engine imports
+from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
+import webapp2
+
+TOKEN = '138455280:AAFp0sPuf4djv507EmpxtuRfPjYsP1JIvUw'
+
+BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
+
+
+# ================================
+
+class EnableStatus(ndb.Model):
+    # key name: str(chat_id)
+    enabled = ndb.BooleanProperty(indexed=False, default=False)
+
+
+# ================================
+
+def setEnabled(chat_id, yes):
+    es = EnableStatus.get_or_insert(str(chat_id))
+    es.enabled = yes
+    es.put()
+
+def getEnabled(chat_id):
+    es = EnableStatus.get_by_id(str(chat_id))
+    if es:
+        return es.enabled
+    return False
+
+
+# ================================
+
+class MeHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getMe'))))
+
+
+class GetUpdatesHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getUpdates'))))
+
+
+class SetWebhookHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        url = self.request.get('url')
+        if url:
+            self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'setWebhook', urllib.urlencode({'url': url})))))
+
+
+class WebhookHandler(webapp2.RequestHandler):
+    def post(self):
+        urlfetch.set_default_fetch_deadline(60)
+        body = json.loads(self.request.body)
+        logging.info('request body:')
+        logging.info(body)
+        self.response.write(json.dumps(body))
+
+        update_id = body['update_id']
+        message = body['message']
+        message_id = message.get('message_id')
+        date = message.get('date')
+        text = message.get('text')
+        fr = message.get('from')
+        chat = message['chat']
+        chat_id = chat['id']
+
+        if not text:
+            logging.info('no text')
+            return
+
+        def reply(msg=None, img=None):
+            if msg:
+                resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({
+                    'chat_id': str(chat_id),
+                    'text': msg.encode('utf-8'),
+                    'disable_web_page_preview': 'false',
+                    'reply_to_message_id': str(message_id),
+                })).read()
+            elif img:
+                resp = multipart.post_multipart(BASE_URL + 'sendPhoto', [
+                    ('chat_id', str(chat_id)),
+                    ('reply_to_message_id', str(message_id)),
+                ], [
+                    ('photo', 'image.jpg', img),
+                ])
+            else:
+                logging.error('no msg or img specified')
+                resp = None
+
+            logging.info('send response:')
+            logging.info(resp)
+
+        # CUSTOMIZE COMMAND HERE  
+        if text.startswith('/'):
+            # START BOT
+            if text == '/start':
+                reply('Bot udah nyala cuy')
+                setEnabled(chat_id, True)
+                
+            # STOP BOT
+            elif text == '/stop':
+                reply('Udahan cuy')
+                setEnabled(chat_id, False)
+                
+            # WIKI COMMAND
+            elif text.startswith('/wiki'):
+                message=text[6:]
+                if not message:
+                    reply('masukin kata kunci pencarian')
+                else:
+                    temprep=wiki(message)
+                    if ("Cannot access link!" in temprep):
+                        message = message.replace (" ", "+")
+                        temprep="Gak ada artikelnya, jangan nge-lely\n"+"https://www.google.co.id/#q="+message
+                    reply(temprep)
+                    
+            # GOOGLE COMMAND
+            elif text.startswith('/ngelely'):
+                message=text[9:]
+                if not message:
+                    reply('masukin kata kunci pencarian')
+                else:
+                    message = message.replace (" ", "+")
+                    reply("jangan nge-lely\n"+"https://www.google.co.id/#q="+message);
+                
+            # ABOUT COMMAND
+            elif text.startswith('/who'):
+                reply('telebot untuk tulul :v\n dibuat dengan python 2.7 dan cinta\n baru bisa ngewiki dan ngelely doang\n'+'kontribusi di https://bitbucket.org/iqbalmineraltown/tululbot/')
+                
+            #ELSE (WRONG COMMAND)
+            else:
+                reply('salah perintah cuy \n'+'liat daftar perintahnya lah');
+
+app = webapp2.WSGIApplication([
+    ('/me', MeHandler),
+    ('/updates', GetUpdatesHandler),
+    ('/set_webhook', SetWebhookHandler),
+    ('/webhook', WebhookHandler),
+], debug=True)
